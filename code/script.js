0d9e95bf6ae25ad5cd0c88e6e0dd4e548841ad72
wallacevieira <wallaceofc@hotmail.com>
2024-08-09 12:45:39 -0300

    feat: desafio concluido

--- code/script.js
+++ code/script.js
@@ -1,1 +1,25 @@
+const tarefas = [];
+function adicionarTarefa(lista, novaTarefa) {
+  if (novaTarefa) {
+    lista.push(novaTarefa);
+    exibirLista(lista);
+  }
+}
 
+function exibirLista(lista) {
+  const taskListElement = document.getElementById("taskList");
+  taskListElement.innerHTML = "";
+  lista.forEach((tarefa, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${index + 1}. ${tarefa}`;
+    taskListElement.appendChild(listItem);
+  });
+}
+
+document.getElementById("addTaskButton").addEventListener("click", () => {
+  const taskInput = document.getElementById("taskInput");
+  const novaTarefa = taskInput.value.trim();
+  adicionarTarefa(tarefas, novaTarefa);
+  taskInput.value = "";
+  taskInput.focus();
+});
